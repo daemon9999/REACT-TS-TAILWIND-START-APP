7103b1447b022305098449b0fb277d9f5b201a55
daemon9999 <murad.mesimli.99@gmail.com>
2023-03-07 21:08:14 +0400

    START APP

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-black h-full w-full flex items-center justify-center text-[60px] font-black tracking-widest text-white">
+      <div className="bg-gray-600  p-20 rounded-lg cursor-pointer hover:bg-gray-800 transition-colors duration-300">REACT-TS-TAILWIND</div>
+    </div>
+  );
+}
+
+export default App;
